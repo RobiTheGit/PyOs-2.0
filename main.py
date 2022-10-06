7adc9e07296dbd4868fd35c1e2f4a744ce658f97
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-10-06 17:29:26 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,57 +5,13 @@ import subprocess
 import colors
 import time
 import getpass
-from datetime import date
 import pyoslogo
-
+import holidays
 correctpass = open('user/password.pass')
 cpass = correctpass.read()
-today = date.today()
-nyd = date(today.year, 12, 31)
-timetilnyd = nyd - today
-white = colors.ccodes[0]
 PyOsLogo = pyoslogo.PyOsLogo
-theme = ''
-username = getpass.getuser()
+username = getpass.getuser()   
 
-if today == date(today.year, 12, 25):
-    todayholiday = 'Christmas'
-    PyOsLogo =pyoslogo.PyOSLogoC
-    
-elif today == date(today.year, 12, 24):
-    todayholiday = 'Christmas Eve'
-    PyOsLogo =pyoslogo.PyOSLogoC
-    
-elif today == date(today.year , 1, 1 ):
-    todayholiday = 'New Years Day'
-    PyOsLogo = pyoslogo.PyOsLogoA
-    
-elif today == nyd:
-    todayholiday = 'New Years Eve'
-    PyOsLogo = pyoslogo.PyOsLogoA
-    
-elif today == date(today.year, 7, 4):
-    todayholiday = 'Fourth of July'
-    c1 = colors.ccodes[6]
-    c2 = colors.ccodes[3]
-    PyOsLogo = pyoslogo.PyOsLogoA
-    
-elif today == date(today.year, 10, 31):
-    todayholiday = 'Halloween'
-    PyOsLogo = pyoslogo.PyOSLogoH
-    
-elif today == date(today.year, 9, 28):
-    todayholiday = "Dad's Birthday"
-    PyOsLogo =pyoslogo.PyOSLogo
-else:
-    todayholiday = ''   
-   
-      
-#*[x] App name*   
-appslist = '''
-
-Press Ctrl*Z to shutdown PyOs (not system)
-'''
 def main():
     subprocess.run('clear')
     print(pyoslogo.PyOsLoginLogo)
@@ -69,25 +25,28 @@ def main():
         main()
 def started():
     subprocess.run('clear')
-    print(f"""
-    {PyOsLogo}                            
-    """)
-    time.sleep(2)
-    subprocess.run('clear')
     os.chdir('user/Apps')
     apps()
     
 
 def apps():
-    print(appslist)
-    print(f'Today is  {today}, Days til the new year, ', abs(timetilnyd.days))
-    print('Holdiays:',todayholiday)
+    print(f"""
+    {PyOsLogo}                            
+    """)
+    print('Press Ctrl*Z to shutdown PyOs (not system)')
+    print(f'Today is  {holidays.today}, Days til the new year, ', abs(holidays.timetilnyd.days))
+    print('Holdiays:',holidays.todayholiday)
     print('Welcome', f'{username}')
     app = input(f'What app would you like to run? ')
     try:
-        subprocess.run('clear')
-        subprocess.run(f"python3 {app}.py", shell=True)
-        recurse()
+        if app+'.py' in os.listdir():
+            subprocess.run('clear')
+            subprocess.run(f"python3 {app}.py", shell=True)
+            recurse()
+        else:
+            print('No app found, check if there is a .py file with that name')
+            time.sleep(0.9)
+            recurse()
     except:
            webbrowser.open("https://github.com/RobiTheGit/PyOs/issues") 
            recurse()
