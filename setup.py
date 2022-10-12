9f9a062ba6c5d77873769bfb42d12867977f2733
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-10-12 14:20:44 -0600

    now it only makes the user/apps folder

--- setup.py
+++ setup.py
@@ -12,11 +12,6 @@ print('This should only be ran when your first get PyOs')
 
 try:
     print('making directories')
-    os.mkdir('user/Downloads')
-    os.mkdir('user/Documents')
-    os.mkdir('user/Pictures')
-    os.mkdir('user/Music')
-    os.mkdir('user/Videos')
     os.mkdir('user/Apps')
     print('All Done with directories')
     setpwd()
