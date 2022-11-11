7d25ae2b7266a93e267c91202f6b8d2eb4dc6284
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-11-11 10:30:13 -0700

    fixed indentation errors

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ print('This should only be ran when your first get PyOs')
 
 try:
     if os.path.exists('user/Apps'):
+        print('directory exists!')
         pass
     else:
         os.mkdir('user/Apps')
@@ -17,26 +18,31 @@ try:
     extra = input('Would you like extra directories such as Downloads? y/N ')
     time.sleep(2)
     if extra == 'Y' or extra == 'y':
-    if os.path.exists('user/Music'):
-        pass
-    else:
-        os.mkdir('user/Music')
-    if os.path.exists('user/Videos'):
-        pass
-    else:
-        os.mkdir('user/Videos')
-    if os.path.exists('user/Documents'):
-        pass
-    else:
-        os.mkdir('user/Documents')
-    if os.path.exists('user/Downloads'):
-        pass
-    else:
-        os.mkdir('user/Downloads')
-    if os.path.exists('user/Pictures'):
-        pass
-    else:
-        os.mkdir('user/Pictures')
+        if os.path.exists('user/Music'):
+            print('directory exists!')
+            pass
+        else:
+            os.mkdir('user/Music')
+        if os.path.exists('user/Videos'):
+            print('directory exists!')
+            pass
+        else:
+            os.mkdir('user/Videos')
+        if os.path.exists('user/Documents'):
+            print('directory exists!')
+            pass
+        else:
+            os.mkdir('user/Documents')
+        if os.path.exists('user/Downloads'):
+            print('directory exists!')
+            pass
+        else:
+            os.mkdir('user/Downloads')
+        if os.path.exists('user/Pictures'):
+            print('directory exists!')
+            pass
+        else:
+            os.mkdir('user/Pictures')
     print('All Done with directories')
     setpwd()
 except:
